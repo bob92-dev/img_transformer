9761ae3cae3b94524d039a50f90ab1882e2279fd
cassel boris <casselboris92@gmail.com>
2019-12-10 16:37:48 +0100

    imports

--- src/main/java/com/boris/img_transformer/JavaCVHelper.java
+++ src/main/java/com/boris/img_transformer/JavaCVHelper.java
@@ -6,7 +6,7 @@ import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_core.Size;
 import org.opencv.core.CvType;
 import org.opencv.imgproc.Imgproc;
-import org.bytedeco.*;
+
 
 
 import java.io.File;
